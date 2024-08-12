vokilam-d/pos-client

Add "Ingredients"; Refactor to use "Ingredients" instead of "Product Options"

src/app/dtos/order-item-ingredient.dto.ts
@@ -1,0 +1,10 @@
+import { Unit } from '../enums/unit.enum';
+
+export class OrderItemIngredientDto {
+  ingredientId: string;
+  name: string;
+  unit: Unit;
+  price: number;
+  qty: number;
+  cost: number;
+}

src/app/pages/admin-category/admin-category.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
 import { PageContentComponent } from '../../components/page-content/page-content.component';
 import { CategoryDto } from '../../dtos/category.dto';
@@ -14,6 +14,12 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { PhotoUploaderComponent } from '../../components/photo-uploader/photo-uploader.component';
 import { environment } from '../../../environments/environment';
 import { PhotoAssetComponent } from '../../components/photo-asset/photo-asset.component';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { RouteDataKey } from '../../enums/route-data-key.enum';
+import { PAGE_ACTION_ADD } from '../../constants';
+import { RouteParamKey } from '../../enums/route-param-key.enum';
 
 type CategoryForm = Record<keyof CreateOrUpdateCategoryDto, FormControl>;
 
@@ -27,17 +33,23 @@ type CategoryForm = Record<keyof CreateOrUpdateCategoryDto, FormControl>;
     PreloaderComponent,
     PhotoUploaderComponent,
     PhotoAssetComponent,
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatLabel,
   ],
   templateUrl: './admin-category.component.html',
   styleUrl: './admin-category.component.scss'
 })
 export class AdminCategoryComponent {
   categoryId = signal<string | null>(null);
-  isNewCategory = computed<boolean>(() => this.categoryId() === 'add');
-
-  form: FormGroup<CategoryForm>;
   category = signal<CategoryDto | CreateOrUpdateCategoryDto>(null);
   isLoading = signal<boolean>(false);
+
+  form: FormGroup<CategoryForm>;
+
+  readonly isNewCategory: boolean = false;
+  readonly isNewCategoryBasedOnId: string = null;
 
   readonly photoUploadUrl = `${environment.apiUrl}/categories/photo`;
 
@@ -46,11 +58,22 @@ export class AdminCategoryComponent {
   private readonly toastr = inject(ToastrService);
   private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
+  private readonly destroyRef = inject(DestroyRef);
 
   constructor() {
-    this.route.params
-      .pipe(takeUntilDestroyed())
-      .subscribe(params => params['categoryId'] ? this.init(params['categoryId']) : null );
+    this.isNewCategory = this.route.snapshot.data[RouteDataKey.PageAction] === PAGE_ACTION_ADD;
+    this.isNewCategoryBasedOnId = this.route.snapshot.params[RouteParamKey.NewItemBasedOn];
+
+    if (this.isNewCategory) {
+      this.init();
+    } else {
+      this.route.params
+        .pipe(takeUntilDestroyed(), )
+        .subscribe(params => {
+          this.categoryId.set(params[RouteParamKey.NewItem]);
+          this.init();
+        });
+    }
   }
 
   save() {
@@ -59,43 +82,46 @@ export class AdminCategoryComponent {
       ...this.form.getRawValue(),
     };
 
-    const request = this.isNewCategory()
+    const request = this.isNewCategory
       ? this.categoryService.create(dto)
       : this.categoryService.update(this.categoryId(), dto);
 
     this.isLoading.set(true);
     request
       .pipe(finalize(() => this.isLoading.set(false)))
-      .subscribe(
-        response => {
-          if (this.isNewCategory()) {
-            this.router.navigate(['..', response.id], { relativeTo: this.route });
+      .subscribe({
+        next: response => {
+          if (this.isNewCategory) {
+            this.router.navigate(['..', response.id], { relativeTo: this.route }).then();
           } else {
             this.category.set(response);
           }
           this.toastr.success(`Успішно збережено`);
         },
-        error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося зберегти категорію`),
-      );
+        error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося зберегти категорію`),
+      });
   }
 
-  private init(categoryId: string) {
-    this.categoryId.set(categoryId);
+  private init() {
+    if (!this.isNewCategory || (this.isNewCategory && this.isNewCategoryBasedOnId)) {
+      const categoryId = (this.isNewCategory && this.isNewCategoryBasedOnId) || this.categoryId();
 
-    if (this.isNewCategory()) {
-      this.category.set(new CreateOrUpdateCategoryDto());
-      this.buildForm();
-    } else {
       this.isLoading.set(true);
-      this.categoryService.fetchCategoryById(this.categoryId())
-        .pipe(finalize(() => this.isLoading.set(false)))
-        .subscribe(
-          response => {
+      this.categoryService.fetchCategoryById(categoryId)
+        .pipe(
+          finalize(() => this.isLoading.set(false)),
+          takeUntilDestroyed(this.destroyRef),
+        )
+        .subscribe({
+          next: response => {
             this.category.set(response);
             this.buildForm();
           },
-          error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося отримати категорію`),
-        );
+          error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося отримати категорію`),
+        });
+    } else {
+      this.category.set(new CreateOrUpdateCategoryDto());
+      this.buildForm();
     }
   }
 
@@ -109,7 +135,7 @@ export class AdminCategoryComponent {
 
   deleteCategory() {
     if (
-      this.isNewCategory()
+      this.isNewCategory
       || !confirm(`Ви впевнені що хочете видалити категорію "${this.category().name}"?`)
     ) {
       return;
@@ -118,13 +144,13 @@ export class AdminCategoryComponent {
     this.isLoading.set(true);
     this.categoryService.deleteCategory(this.categoryId())
       .pipe(finalize(() => this.isLoading.set(false)))
-      .subscribe(
-        () => {
-          this.router.navigate(['..'], { relativeTo: this.route });
+      .subscribe({
+        next: () => {
+          this.router.navigate(['..'], { relativeTo: this.route }).then();
           this.toastr.success(`Успішно видалено`, undefined);
         },
-        error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося видалити категорію`),
-      );
+        error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося видалити категорію`),
+      });
   }
 
   onPhotoUpload(photoUrl: string): void {

src/app/pages/admin-ingredient/admin-ingredient.component.ts
@@ -1,0 +1,174 @@
+import { Component, inject, signal } from '@angular/core';
+import { IngredientService } from '../../services/ingredient.service';
+import { PageContentComponent } from '../../components/page-content/page-content.component';
+import { IngredientDto } from '../../dtos/ingredient.dto';
+import { CreateOrUpdateIngredientDto } from '../../dtos/create-or-update-ingredient.dto';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { getHttpErrorMessage } from '../../utils/get-http-error-message.util';
+import { finalize } from 'rxjs';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { PreloaderComponent } from '../../components/page-preloader/preloader.component';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Unit } from '../../enums/unit.enum';
+import { IngredientUnitPipe } from '../../pipes/ingredient-unit.pipe';
+import { MatAnchor, MatButton } from '@angular/material/button';
+import { MatOption } from '@angular/material/core';
+import { MatSelect } from '@angular/material/select';
+import { PAGE_ACTION_ADD } from '../../constants';
+import { RouteDataKey } from '../../enums/route-data-key.enum';
+import { RouteParamKey } from '../../enums/route-param-key.enum';
+
+class IngredientForm implements Record<keyof CreateOrUpdateIngredientDto, unknown> {
+  name: FormControl<string>;
+  price: FormControl<number>;
+  qty: FormControl<number>;
+  unit: FormControl<Unit>;
+}
+
+@Component({
+  selector: 'app-admin-ingredient',
+  standalone: true,
+  imports: [
+    PageContentComponent,
+    MatProgressSpinner,
+    ReactiveFormsModule,
+    PreloaderComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    IngredientUnitPipe,
+    MatOption,
+    MatSelect,
+    MatButton,
+    MatAnchor,
+    RouterLink,
+  ],
+  templateUrl: './admin-ingredient.component.html',
+  styleUrl: './admin-ingredient.component.scss'
+})
+export class AdminIngredientComponent {
+  readonly ingredientService = inject(IngredientService);
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly toastr = inject(ToastrService);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
+
+  ingredientId = signal<string | null>(null);
+  ingredient = signal<IngredientDto>(null);
+  isLoading = signal<boolean>(false);
+
+  isNewIngredient: boolean = false;
+  isNewIngredientBasedOnId: string = null;
+  form: FormGroup<IngredientForm>;
+  units = Object.values<Unit>(Unit);
+
+  constructor() {
+    this.isNewIngredient = this.route.snapshot.data[RouteDataKey.PageAction] === PAGE_ACTION_ADD;
+    this.isNewIngredientBasedOnId = this.route.snapshot.params[RouteParamKey.NewItemBasedOn];
+
+    if (this.isNewIngredient) {
+      this.init();
+    } else {
+      this.route.params
+        .pipe(takeUntilDestroyed(), )
+        .subscribe(params => {
+          this.ingredientId.set(params[RouteParamKey.NewItem]);
+          this.init();
+        });
+    }
+  }
+
+  getBackToListUrl(): string[] {
+    let commands: string = '../';
+    if (this.isNewIngredient && this.isNewIngredientBasedOnId) {
+      commands += '../';
+    }
+    return [commands];
+  }
+
+  save(postAction?: 'close') {
+    const dto: CreateOrUpdateIngredientDto = {
+      ...this.ingredient(),
+      ...this.form.getRawValue(),
+    };
+
+    const request = this.isNewIngredient
+      ? this.ingredientService.create(dto)
+      : this.ingredientService.update(this.ingredientId(), dto);
+
+    this.isLoading.set(true);
+    request
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: response => {
+          this.toastr.success(`Успішно збережено`);
+
+          if (postAction === 'close') {
+            this.router.navigate(this.getBackToListUrl(), { relativeTo: this.route }).then();
+            return;
+          }
+
+          if (this.isNewIngredient) {
+            const url = [this.getBackToListUrl(), response.id].join('');
+            this.router.navigate([url], { relativeTo: this.route }).then();
+          } else {
+            this.ingredient.set(response);
+            this.buildForm();
+          }
+        },
+        error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося зберегти інгредієнт`),
+      });
+  }
+
+  private init() {
+    if (!this.isNewIngredient || (this.isNewIngredient && this.isNewIngredientBasedOnId)) {
+      const ingredientId = (this.isNewIngredient && this.isNewIngredientBasedOnId) || this.ingredientId();
+
+      this.isLoading.set(true);
+      this.ingredientService.fetchIngredientById(ingredientId)
+        .pipe(finalize(() => this.isLoading.set(false)))
+        .subscribe({
+          next: response => {
+            this.ingredient.set(response);
+            this.buildForm();
+          },
+          error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося отримати інгредієнт`),
+        });
+    } else {
+      this.ingredient.set(new IngredientDto());
+      this.buildForm();
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group<IngredientForm>({
+      name: this.formBuilder.control(this.ingredient().name),
+      qty: this.formBuilder.control(this.ingredient().qty),
+      price: this.formBuilder.control(this.ingredient().price),
+      unit: this.formBuilder.control(this.ingredient().unit),
+    });
+  }
+
+  deleteIngredient() {
+    if (
+      this.isNewIngredient
+      || !confirm(`Ви впевнені що хочете видалити інгредієнт "${this.ingredient().name}"?`)
+    ) {
+      return;
+    }
+
+    this.isLoading.set(true);
+    this.ingredientService.deleteIngredient(this.ingredientId())
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: () => {
+          this.router.navigate(['..'], { relativeTo: this.route }).then();
+          this.toastr.success(`Успішно видалено`, undefined);
+        },
+        error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося видалити інгредієнт`),
+      });
+  }
+}

vokilam-d/posh-client

src/app/dtos/order-item-ingredient.dto.ts
@@ -1,0 +1,10 @@
+import { Unit } from '../enums/unit.enum';
+
+export class OrderItemIngredientDto {
+  ingredientId: string;
+  name: string;
+  unit: Unit;
+  price: number;
+  qty: number;
+  totalPrice: number;
+}

src/app/pages/admin-category/admin-category.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
 import { PageContentComponent } from '../../components/page-content/page-content.component';
 import { CategoryDto } from '../../dtos/category.dto';
@@ -14,6 +14,12 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { PhotoUploaderComponent } from '../../components/photo-uploader/photo-uploader.component';
 import { environment } from '../../../environments/environment';
 import { PhotoAssetComponent } from '../../components/photo-asset/photo-asset.component';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { RouteDataKey } from '../../enums/route-data-key.enum';
+import { PAGE_ACTION_ADD } from '../../constants';
+import { RouteParamKey } from '../../enums/route-param-key.enum';
 
 type CategoryForm = Record<keyof CreateOrUpdateCategoryDto, FormControl>;
 
@@ -27,17 +33,23 @@ type CategoryForm = Record<keyof CreateOrUpdateCategoryDto, FormControl>;
     PreloaderComponent,
     PhotoUploaderComponent,
     PhotoAssetComponent,
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatLabel,
   ],
   templateUrl: './admin-category.component.html',
   styleUrl: './admin-category.component.scss'
 })
 export class AdminCategoryComponent {
   categoryId = signal<string | null>(null);
-  isNewCategory = computed<boolean>(() => this.categoryId() === 'add');
-
-  form: FormGroup<CategoryForm>;
   category = signal<CategoryDto | CreateOrUpdateCategoryDto>(null);
   isLoading = signal<boolean>(false);
+
+  form: FormGroup<CategoryForm>;
+
+  readonly isNewCategory: boolean = false;
+  readonly isNewCategoryBasedOnId: string = null;
 
   readonly photoUploadUrl = `${environment.apiUrl}/categories/photo`;
 
@@ -46,11 +58,22 @@ export class AdminCategoryComponent {
   private readonly toastr = inject(ToastrService);
   private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
+  private readonly destroyRef = inject(DestroyRef);
 
   constructor() {
-    this.route.params
-      .pipe(takeUntilDestroyed())
-      .subscribe(params => params['categoryId'] ? this.init(params['categoryId']) : null );
+    this.isNewCategory = this.route.snapshot.data[RouteDataKey.PageAction] === PAGE_ACTION_ADD;
+    this.isNewCategoryBasedOnId = this.route.snapshot.params[RouteParamKey.ItemIdBasedOn];
+
+    if (this.isNewCategory) {
+      this.init();
+    } else {
+      this.route.params
+        .pipe(takeUntilDestroyed(), )
+        .subscribe(params => {
+          this.categoryId.set(params[RouteParamKey.ItemId]);
+          this.init();
+        });
+    }
   }
 
   save() {
@@ -59,43 +82,46 @@ export class AdminCategoryComponent {
       ...this.form.getRawValue(),
     };
 
-    const request = this.isNewCategory()
+    const request = this.isNewCategory
       ? this.categoryService.create(dto)
       : this.categoryService.update(this.categoryId(), dto);
 
     this.isLoading.set(true);
     request
       .pipe(finalize(() => this.isLoading.set(false)))
-      .subscribe(
-        response => {
-          if (this.isNewCategory()) {
-            this.router.navigate(['..', response.id], { relativeTo: this.route });
+      .subscribe({
+        next: response => {
+          if (this.isNewCategory) {
+            this.router.navigate(['..', response.id], { relativeTo: this.route }).then();
           } else {
             this.category.set(response);
           }
           this.toastr.success(`Успішно збережено`);
         },
-        error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося зберегти категорію`),
-      );
+        error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося зберегти категорію`),
+      });
   }
 
-  private init(categoryId: string) {
-    this.categoryId.set(categoryId);
+  private init() {
+    if (!this.isNewCategory || (this.isNewCategory && this.isNewCategoryBasedOnId)) {
+      const categoryId = (this.isNewCategory && this.isNewCategoryBasedOnId) || this.categoryId();
 
-    if (this.isNewCategory()) {
-      this.category.set(new CreateOrUpdateCategoryDto());
-      this.buildForm();
-    } else {
       this.isLoading.set(true);
-      this.categoryService.fetchCategoryById(this.categoryId())
-        .pipe(finalize(() => this.isLoading.set(false)))
-        .subscribe(
-          response => {
+      this.categoryService.fetchCategoryById(categoryId)
+        .pipe(
+          finalize(() => this.isLoading.set(false)),
+          takeUntilDestroyed(this.destroyRef),
+        )
+        .subscribe({
+          next: response => {
             this.category.set(response);
             this.buildForm();
           },
-          error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося отримати категорію`),
-        );
+          error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося отримати категорію`),
+        });
+    } else {
+      this.category.set(new CreateOrUpdateCategoryDto());
+      this.buildForm();
     }
   }
 
@@ -109,7 +135,7 @@ export class AdminCategoryComponent {
 
   deleteCategory() {
     if (
-      this.isNewCategory()
+      this.isNewCategory
       || !confirm(`Ви впевнені що хочете видалити категорію "${this.category().name}"?`)
     ) {
       return;
@@ -118,13 +144,13 @@ export class AdminCategoryComponent {
     this.isLoading.set(true);
     this.categoryService.deleteCategory(this.categoryId())
       .pipe(finalize(() => this.isLoading.set(false)))
-      .subscribe(
-        () => {
-          this.router.navigate(['..'], { relativeTo: this.route });
+      .subscribe({
+        next: () => {
+          this.router.navigate(['..'], { relativeTo: this.route }).then();
           this.toastr.success(`Успішно видалено`, undefined);
         },
-        error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося видалити категорію`),
-      );
+        error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося видалити категорію`),
+      });
   }
 
   onPhotoUpload(photoUrl: string): void {

src/app/pages/admin-ingredient/admin-ingredient.component.ts
@@ -1,0 +1,174 @@
+import { Component, inject, signal } from '@angular/core';
+import { IngredientService } from '../../services/ingredient.service';
+import { PageContentComponent } from '../../components/page-content/page-content.component';
+import { IngredientDto } from '../../dtos/ingredient.dto';
+import { CreateOrUpdateIngredientDto } from '../../dtos/create-or-update-ingredient.dto';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { getHttpErrorMessage } from '../../utils/get-http-error-message.util';
+import { finalize } from 'rxjs';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { PreloaderComponent } from '../../components/page-preloader/preloader.component';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Unit } from '../../enums/unit.enum';
+import { IngredientUnitPipe } from '../../pipes/ingredient-unit.pipe';
+import { MatAnchor, MatButton } from '@angular/material/button';
+import { MatOption } from '@angular/material/core';
+import { MatSelect } from '@angular/material/select';
+import { PAGE_ACTION_ADD } from '../../constants';
+import { RouteDataKey } from '../../enums/route-data-key.enum';
+import { RouteParamKey } from '../../enums/route-param-key.enum';
+
+class IngredientForm implements Record<keyof CreateOrUpdateIngredientDto, unknown> {
+  name: FormControl<string>;
+  price: FormControl<number>;
+  qty: FormControl<number>;
+  unit: FormControl<Unit>;
+}
+
+@Component({
+  selector: 'app-admin-ingredient',
+  standalone: true,
+  imports: [
+    PageContentComponent,
+    MatProgressSpinner,
+    ReactiveFormsModule,
+    PreloaderComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    IngredientUnitPipe,
+    MatOption,
+    MatSelect,
+    MatButton,
+    MatAnchor,
+    RouterLink,
+  ],
+  templateUrl: './admin-ingredient.component.html',
+  styleUrl: './admin-ingredient.component.scss'
+})
+export class AdminIngredientComponent {
+  readonly ingredientService = inject(IngredientService);
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly toastr = inject(ToastrService);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
+
+  ingredientId = signal<string | null>(null);
+  ingredient = signal<IngredientDto>(null);
+  isLoading = signal<boolean>(false);
+
+  isNewIngredient: boolean = false;
+  isNewIngredientBasedOnId: string = null;
+  form: FormGroup<IngredientForm>;
+  units = Object.values<Unit>(Unit);
+
+  constructor() {
+    this.isNewIngredient = this.route.snapshot.data[RouteDataKey.PageAction] === PAGE_ACTION_ADD;
+    this.isNewIngredientBasedOnId = this.route.snapshot.params[RouteParamKey.ItemIdBasedOn];
+
+    if (this.isNewIngredient) {
+      this.init();
+    } else {
+      this.route.params
+        .pipe(takeUntilDestroyed(), )
+        .subscribe(params => {
+          this.ingredientId.set(params[RouteParamKey.ItemId]);
+          this.init();
+        });
+    }
+  }
+
+  getBackToListUrl(): string[] {
+    let commands: string = '../';
+    if (this.isNewIngredient && this.isNewIngredientBasedOnId) {
+      commands += '../';
+    }
+    return [commands];
+  }
+
+  save(postAction?: 'close') {
+    const dto: CreateOrUpdateIngredientDto = {
+      ...this.ingredient(),
+      ...this.form.getRawValue(),
+    };
+
+    const request = this.isNewIngredient
+      ? this.ingredientService.create(dto)
+      : this.ingredientService.update(this.ingredientId(), dto);
+
+    this.isLoading.set(true);
+    request
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: response => {
+          this.toastr.success(`Успішно збережено`);
+
+          if (postAction === 'close') {
+            this.router.navigate(this.getBackToListUrl(), { relativeTo: this.route }).then();
+            return;
+          }
+
+          if (this.isNewIngredient) {
+            const url = [this.getBackToListUrl(), response.id].join('');
+            this.router.navigate([url], { relativeTo: this.route }).then();
+          } else {
+            this.ingredient.set(response);
+            this.buildForm();
+          }
+        },
+        error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося зберегти інгредієнт`),
+      });
+  }
+
+  private init() {
+    if (!this.isNewIngredient || (this.isNewIngredient && this.isNewIngredientBasedOnId)) {
+      const ingredientId = (this.isNewIngredient && this.isNewIngredientBasedOnId) || this.ingredientId();
+
+      this.isLoading.set(true);
+      this.ingredientService.fetchIngredientById(ingredientId)
+        .pipe(finalize(() => this.isLoading.set(false)))
+        .subscribe({
+          next: response => {
+            this.ingredient.set(response);
+            this.buildForm();
+          },
+          error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося отримати інгредієнт`),
+        });
+    } else {
+      this.ingredient.set(new IngredientDto());
+      this.buildForm();
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group<IngredientForm>({
+      name: this.formBuilder.control(this.ingredient().name),
+      qty: this.formBuilder.control(this.ingredient().qty),
+      price: this.formBuilder.control(this.ingredient().price),
+      unit: this.formBuilder.control(this.ingredient().unit),
+    });
+  }
+
+  deleteIngredient() {
+    if (
+      this.isNewIngredient
+      || !confirm(`Ви впевнені що хочете видалити інгредієнт "${this.ingredient().name}"?`)
+    ) {
+      return;
+    }
+
+    this.isLoading.set(true);
+    this.ingredientService.deleteIngredient(this.ingredientId())
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: () => {
+          this.router.navigate(['..'], { relativeTo: this.route }).then();
+          this.toastr.success(`Успішно видалено`, undefined);
+        },
+        error: error => this.toastr.error(getHttpErrorMessage(error), `Не вдалося видалити інгредієнт`),
+      });
+  }
+}